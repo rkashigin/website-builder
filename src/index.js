675d23ec48035e0c43df1f51db71a078fe44d6ef
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 09:06:45 +0500

    Model and functions transfered to separate modules

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-const model = [
-    {
-        type: 'title',
-        value: 'Welcome to my website-builder',
-    },
-    {
-        type: 'text',
-        value: 'Text placeholder',
-    },
-    {
-        type: 'columns',
-        value: [
-            '111111111',
-            '222222222',
-            '333333333',
-            '444444444',
-            '555555555',
-        ],
-    },
-    {
-        type: 'image',
-        value: './assets/img.png'
-    },
-];
+import {model} from "./model";
+import {title, text, columns, image} from "./templates";
+import './styles/main.css';
 
 const $website = document.querySelector('#website');
 
@@ -40,40 +19,3 @@ model.forEach(block => {
 
     $website.insertAdjacentHTML('beforeend', html);
 });
-
-function title(block) {
-    return `
-            <div class="row">
-                <div class="col-sm">
-                    <h1>${block.value}</h1>
-                </div>
-            </div>
-        `;
-}
-
-function text(block) {
-    return `
-            <div class="row">
-                <div class="col-sm">
-                    <p>${block.value}</p>
-                </div>
-            </div>
-        `;
-}
-
-function columns(block) {
-    const html = block.value.map(item => `<div class="col-sm">${item}</div>`);
-    return `
-        <div class="row">
-            ${html.join('')}
-        </div>
-    `;
-}
-
-function image(block) {
-    return `
-        <div class="row">
-            <img src="${block.value}"/>
-        </div>
-    `;
-}
