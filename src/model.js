d8cc692a6a6927d4832f6c14dfa94011573eb754
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 09:07:07 +0500

    Module for model variable

---
+++ src/model.js
@@ -0,0 +1,24 @@
+export const model = [
+    {
+        type: 'title',
+        value: 'Welcome to my website-builder',
+    },
+    {
+        type: 'text',
+        value: 'Text placeholder',
+    },
+    {
+        type: 'columns',
+        value: [
+            '111111111',
+            '222222222',
+            '333333333',
+            '444444444',
+            '555555555',
+        ],
+    },
+    {
+        type: 'image',
+        value: './assets/img.png'
+    },
+];
