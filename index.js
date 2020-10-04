e32903f970e15954083b02671137f31475a2e9e9
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 08:17:46 +0500

    Added array of content blocks

---
+++ index.js
@@ -0,0 +1,18 @@
+const model = [
+    {
+        type: 'title',
+        value: 'Welcome to my website-builder',
+    },
+    {
+        type: 'text',
+        value: 'Text placeholder',
+    },
+    {
+        type: 'columns',
+        value: [
+            '111111111',
+            '222222222',
+            '333333333',
+        ],
+    }
+];
