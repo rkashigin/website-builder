16a6bbb9040c7fbb2c15eec190d825a1a8710dc7
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 09:25:06 +0500

    Generation functions rewrote using util functions

--- src/templates.js
+++ src/templates.js
@@ -1,38 +1,20 @@
+import {row, col} from "./utils";
+
 function title(block) {
-    return `
-            <div class="row">
-                <div class="col-sm">
-                    <h1>${block.value}</h1>
-                </div>
-            </div>
-        `;
+    return row(col(`<h1>${block.value}</h1>`));
 }
 
 function text(block) {
-    return `
-            <div class="row">
-                <div class="col-sm">
-                    <p>${block.value}</p>
-                </div>
-            </div>
-        `;
+    return row(col(`<p>${block.value}</p>`));
 }
 
 function columns(block) {
-    const html = block.value.map(item => `<div class="col-sm">${item}</div>`);
-    return `
-        <div class="row">
-            ${html.join('')}
-        </div>
-    `;
+    const html = block.value.map(item => col(item));
+    return row(html.join(''));
 }
 
 function image(block) {
-    return `
-        <div class="row">
-            <img src="${block.value}"/>
-        </div>
-    `;
+    return row(`<img src="${block.value}"/>`);
 }
 
 export const templates = {
