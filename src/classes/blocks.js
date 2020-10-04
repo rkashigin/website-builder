434baa2a800ab6d6a33982708bce3a1416cd9c0c
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 10:32:47 +0500

    Deleted templates file and removed titles (now all work through classes)

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -1,31 +1,55 @@
+import {col, css, row} from "../utils";
+
 class Block {
-    constructor(type, value, options) {
-        this.type = type;
+    constructor(value, options) {
         this.value = value;
         this.options = options;
+    }
+
+    toHTML() {
+        throw new Error('Метод toHTML должен быть реализован!');
     }
 }
 
 export class TitleBlock extends Block {
     constructor(value, options) {
-        super('title', value, options);
+        super(value, options);
+    }
+
+    toHTML() {
+        const {tag = 'h1', styles} = this.options;
+        return row(col(`<${tag}>${this.value}</${tag}>`), css(styles));
     }
 }
 
 export class ImageBlock extends Block {
     constructor(value, options) {
-        super('image', value, options);
+        super(value, options);
+    }
+
+    toHTML() {
+        const {imageStyles: is, alt = '', styles} = this.options;
+        return row(`<img src="${this.value}" alt="${alt}" style="${css(is)}"/>`, css(styles));
     }
 }
 
 export class ColumnsBlock extends Block {
     constructor(value, options) {
-        super('columns', value, options);
+        super(value, options);
+    }
+
+    toHTML() {
+        const html = this.value.map(col).join('');
+        return row(html, css(this.options.styles));
     }
 }
 
 export class TextBlock extends Block {
     constructor(value, options) {
-        super('text', value, options);
+        super(value, options);
+    }
+
+    toHTML() {
+        return row(col(`<p>${this.value}</p>`), css(this.options.styles));
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import {model} from "./model";
-import {templates} from "./templates";
 import './styles/main.css';
 
 const $website = document.querySelector('#website');
 
 model.forEach(block => {
-
-    const toHTML = templates[block.type];
-
-    if (toHTML) {
-        $website.insertAdjacentHTML('beforeend', toHTML(block));
-    }
-
+    $website.insertAdjacentHTML('beforeend', block.toHTML());
 });
